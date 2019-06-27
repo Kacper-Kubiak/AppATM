38cbf237e7249d4b43cd3d7e646d070dec41f7d5
kacper <mYdTka2sk5>
2019-06-27 11:54:15 +0200

    Dziala Currency2

--- app/src/main/java/pl/pwr/s230473/appatm/ExchangeFragment.java
+++ app/src/main/java/pl/pwr/s230473/appatm/ExchangeFragment.java
@@ -6,26 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ExchangeFragment extends Fragment {
     private static final String TAG = "ExchangeFragment";
 
-    private Button btnTEST;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.exchange_fragment,container,false);
-        btnTEST = view.findViewById(R.id.btnTEST);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
-            }
-        });
+        /*Spinner spinnerFrom = view.findViewById(R.id.spinnerFrom);
+        Spinner spinnerTo = view.findViewById(R.id.spinnerTo);
+        EditText textValueFrom = view.findViewById(R.id.valueFrom);
+        EditText textValueTo = view.findViewById(R.id.valueTo);
+
+        Currency*/
 
         return view;
     }
